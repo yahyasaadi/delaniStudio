76ab3e7ec6b943b69ffe28f9599227bab17a94d8
yahyasaadi <yahyasaadi9219@gmail.com>
2021-11-22 10:44:05 +0300

    Added code for the feedback form

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,17 @@ $().ready(function(){
     $("#proImg").click(function(){
         $("#pProduct").toggle();
     });
+
+
+    // Code for the user form
+    var $newForm = $("#form");
+    var $nameInput = $('input:text');
+
+    $newForm.on('submit', function(e){
+        e.preventDefault();
+        var name = $nameInput.val();
+        alert(name + " we have received your message. Thank you for reaching out to us.");
+    });
+
+    
 });
